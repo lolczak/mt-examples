9cd6439b26d2f39e422aede9a6020900db771cbe
lolczak <lukasz.jan.olczak@gmail.com>
2016-01-16 22:19:24 +0100

    simple RWST

--- src/main/scala/org/lolczak/mt/idiomatic/package.scala
+++ src/main/scala/org/lolczak/mt/idiomatic/package.scala
@@ -4,32 +4,32 @@ import org.lolczak.lambda._
 
 import scalaz._
 import scalaz.effect.IO
+import Scalaz._
 
 package object idiomatic {
 
   type StateType = Int
 
   type Logs = List[String]
-  type Eval[A] = EitherT[RWST[IO, Env, Logs, StateType, ?], A, Failure]
+  type Eval[A] = RWST[IO, Env, Logs, StateType, A]
 
-  def runEval[A](env: Env, st: StateType, op: Eval[A]): IO[(Logs, A \/ Failure, StateType)] = {
-    val rwst: RWST[IO, Env, Logs, StateType, A \/ Failure] = op.run //F[A \/ B]
-    rwst.run(env, st)
+  def runEval[A](env: Env, st: StateType, op: Eval[A]): IO[(Logs, A, StateType)] = {
+    op.run(env, st)
   }
 
-//  def eval(exp: Exp): Eval[LambdaValue] = exp match {
-//    case Lit(i) =>
-//      for {
-//        _ <- tick[Eval]
-////        _ <- IO.putStrLn(s"Lit $i").liftIO
-//      } yield IntVal(i)
-//  }
-//
-//  def tick[F[_]](implicit M: MonadState[F, Int]): F[Unit] =
-//    for {
-//      st <- M.get
-//      _ <- M.put(st + 1)
-//    } yield ()
+  def eval(exp: Exp): Eval[LambdaValue] = exp match {
+    case Lit(i) =>
+      for {
+        _ <- tick[Eval]
+//        _ <- IO.putStrLn(s"Lit $i").liftIO
+      } yield IntVal(i).asInstanceOf[LambdaValue]
+  }
+
+  def tick[F[_]](implicit M: MonadState[F, Int]): F[Unit] =
+    for {
+      st <- M.get
+      _  <- M.put(st + 1)
+    } yield ()
 
 
   case class Failure(msg: String)
