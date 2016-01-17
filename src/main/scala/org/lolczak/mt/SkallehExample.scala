dd7ad527c214e0548307e5111f33f86e04087034
lolczak <lukasz.jan.olczak@gmail.com>
2016-01-17 12:24:32 +0100

    refactoring

---
+++ src/main/scala/org/lolczak/mt/SkallehExample.scala
@@ -0,0 +1,23 @@
+package org.lolczak.mt
+
+import org.lolczak.lambda._
+
+import scalaz._
+import scalaz.effect.IO
+
+object SkallehExample {
+  type StateType = Int
+
+  type Logs = List[String]
+  type Eval[A] = ReaderT[EitherT[WriterT[StateT[IO, StateType, ?], Logs, ?], ?, Failure], Env, A]
+
+  def runEval[A](env: Env, st: StateType, op: Eval[A]): IO[(StateType, (Logs, A \/ Failure))] = {
+    val either: EitherT[WriterT[StateT[IO, StateType, ?], Logs, ?], A, Failure] = op.run(env)
+    val writer: WriterT[StateT[IO, StateType, ?], Logs, A \/ Failure]           = either.run
+    val state:  StateT[IO, StateType, (Logs, A \/ Failure)]                     = writer.run
+    val task:   IO[(StateType, (Logs, A \/ Failure))]                           = state.run(st)
+    task
+  }
+
+  case class Failure(msg: String)
+}
